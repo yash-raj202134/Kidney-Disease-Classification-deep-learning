d00908e0e6214782768090f03bc837da98d048cd
yash-raj202134 <yashraj3376@gmail.com>
2024-03-20 18:07:54 +0530

    UI and pipeline update

---
+++ src/cnnClassifier/pipeline/prediction.py
@@ -0,0 +1,33 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+import os
+
+
+
+class PredictionPipeline:
+    def __init__(self,filename):
+        self.filename =filename
+
+
+    
+    def predict(self):
+        # load model
+
+        model = tf.keras.models.load_model(os.path.join("model", "model.h5"))
+        # print(os.path.exists(model))
+
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size = (224,224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = np.argmax(model.predict(test_image), axis=1)
+        print(result)
+
+        if result[0] == 1:
+            prediction = 'Tumor'
+            return [{ "image" : prediction}]
+        else:
+            prediction = 'Normal'
+            return [{ "image" : prediction}]
